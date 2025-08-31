9004feef6334bf39aefab0b231786bfbdb30c37d
Licia Mozzina <lmozzina@lxplus912.cern.ch>
2025-08-31 22:07:52 +0200

    Added fit to HT

--- RecTrack.py
+++ RecTrack.py
@@ -7,11 +7,6 @@ import ROOT
 CellLength = 4.2 # cm
 CellHeight = 1.3 # cm
 
-# c_values = 1000
-m_values = 1000
-
-# termina descrizione hough
-
 class RecTrack:
     """
     --------
@@ -48,7 +43,19 @@ class RecTrack:
     
     ResidualHitWireDistance(self): this function computes the difference between the measured horizontal hit position and the expected hit position from the track fitting. It returns this difference and the distance of the hit orizontal position from the corresponding cell's anodic wire
     
-    HoughFit(self):
+    HoughFit(self): this function exploits a Hough Transform (HT) method fit on the hits selected by the FitTPLateralities() method. 
+                    For each hit, it computes the function c = x[i] + m * y, where x and y are the hit.Position values (both the left and 
+                    right x options are considered), and both m and c are unknown. More precisely, m is the inverse angular coefficient of 
+                    the RecTrack and c is the track interception on the lower bound of the cell (i.e. at y = 0). Hits belonging to the same 
+                    track will share the same m and c values, therefore the corresponding bin of the 2d histogram of m and c values, the so-
+                    called 'Hough accumulator' will be the most populated and will be used to compute the RecTrack values. 
+                    The range of values for m and c that will define the histogram bounds are still under optimization. For now, the bins
+                    for m values are 20 mrad wide, in the [-60°, +60°] interval circa wrt the normal to the MiniDT layers, 
+                    while the bins for c values are 2 mm wide, covering a length of approximately 1 m.
+                    -----------
+                    CAVEAT
+                    -----------
+                    The chi-square value for this method is not computed, and set to 0 by default.
     
     
     """
@@ -139,8 +146,7 @@ class RecTrack:
             
             plt.axis('scaled')
                 
-#         plt.axline((0, (self.Intercept)), slope = self.Slope, linewidth = 1, color = 'mediumvioletred')
-        plt.axline((self.XIntercept, 0), slope = self.Slope, linewidth = 1, color = 'mediumvioletred') # CORREGGIIII
+        plt.axline((0, (self.Intercept)), slope = self.Slope, linewidth = 1, color = 'mediumvioletred')
         plt.show()
         print(f"Slope: {self.Slope}, Intercept: {self.XIntercept} cm, ChiSquare: {self.ChiSquare}")
        
@@ -156,7 +162,7 @@ class RecTrack:
             else:
                 return(LayerIntercept - x, abs(x - x_offset))
             
-    def HoughFit(self, c_values):
+    def HoughFit(self, c_values, m_values):
         accumulator = np.zeros((c_values, m_values))
         dm = 4 / m_values
 
@@ -177,7 +183,7 @@ class RecTrack:
                     c_plot.append(c)
 
         accumulator_max = np.argmax(accumulator, axis=None)
-        if accumulator_max < 3:
+        if accumulator_max < 3: 
             m_track = 999
             c_track = 999
         else:
@@ -185,15 +191,34 @@ class RecTrack:
             m_track = accumulator_peak[1] * dm - 2
             c_track = accumulator_peak[0] / (c_values / 100)
 
-        hist, xedges, yedges, image = plt.hist2d(m_plot, c_plot, bins=[1000,1000], range=[[-2, 2],[-20, 100]])
+        hist, xedges, yedges, image = plt.hist2d(m_plot, c_plot, bins=[1000,1000], range=[[-2, 2],[-20, 80]])
         plt.colorbar()
         plt.show()
-    #     plt.savefig(f"/eos/user/l/lmozzina/MiniDT/matching/run_011049/hough/accumulators/accumulator_{n_event}_{n_track}.png")
         plt.clf()
 
-        self.Slope = - 1 / m_track
-        self.XIntercept = c_track # SBAGLIATA, DOVREBBE ESSERE tra 2 e 3 layer
+        HT_track_slope = - 1 / m_track
+        HT_track_intercept = - c_track * HT_track_slope 
         
-            # fit
+        SelectedHits = []
+        for layer in range(4):
+            x = (((layer + 0.5) * CellHeight) - HT_track_intercept) / HT_track_slope
+            layer_hits = [hit for hit in self.Hits if hit.Layer == layer]
+            x_diff_0 = [(abs(x - hit.Position[0][0]) if abs(x - hit.Position[0][0]) < CellLength / 2 else 1000) for hit in layer_hits]
+            x_diff_1 = [(abs(x - hit.Position[1][0]) if abs(x - hit.Position[1][0]) < CellLength / 2 else 1000) for hit in layer_hits]
+
+            if (len(x_diff_0) != 0):
+                if (min(x_diff_0) < min(x_diff_1)):
+                    layer_hits[x_diff_0.index(min(x_diff_0))].Laterality = 0
+                    SelectedHits.append(layer_hits[x_diff_0.index(min(x_diff_0))]) 
+                else:
+                    layer_hits[x_diff_1.index(min(x_diff_1))].Laterality = 1
+                    SelectedHits.append(layer_hits[x_diff_1.index(min(x_diff_1))]) 
+
+        self.Hits = SelectedHits
+        self.Slope = HT_track_slope 
+        self.Intercept = HT_track_intercept
+        self.XIntercept = (2 * CellHeight - HT_track_intercept) / HT_track_slope
+        self.ChiSquare = 0
+
 
         
